234ae585b1be37ebfea50ab93d8007afedd20e1f
catc <catalin.covic@gmail.com>
2022-03-31 14:55:06 -0700

    bump version

--- debug.d.ts
+++
@@ -1,6 +0,0 @@
-import { ASTPath, Node } from 'jscodeshift';
-interface Options {
-    spacing: number;
-}
-export declare function debug(n: Node | ASTPath<Node>, options?: Options): void;
-export {};
